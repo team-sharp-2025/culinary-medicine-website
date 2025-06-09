98bab7fb5ea463bf46bff5d57f2915f390bcb7bb
RagulRaj-S <ragulraju218@gmail.com>
2025-06-09 20:21:36 +0530

    Updated fetch all with search functionality with pagination

--- apps/admin/src/app/api/blogs/route.ts
+++ apps/admin/src/app/api/blogs/route.ts
@@ -2,16 +2,6 @@ import { NextResponse } from 'next/server';
 import { ApiResponse } from '../../../../../../packages/lib/src/ApiResponse';
 import { blogService } from '../../../../../../packages/lib/src/services/blogService';
 
-export async function GET() {
-  try {
-    const blogs = await blogService.getAll();
-    return NextResponse.json(ApiResponse.success(blogs));
-  } catch (error) {
-    console.error("Error occured while fetching all the blog: ", error);
-    return NextResponse.json(ApiResponse.error('Failed to fetch blogs'), { status: 500 });
-  }
-}
-
 export async function POST(req: Request) {
   try {
     const { title, content } = await req.json()
--- packages/lib/src/services/blogService.ts
+++ packages/lib/src/services/blogService.ts
@@ -1,8 +1,28 @@
 import { prisma } from '../prisma';
 
 export const blogService = {
-    async getAll() {
-        return prisma.blog.findMany();
+    async getPaginated(skip: number, take: number, search: string) {
+        return prisma.blog.findMany({
+            where: {
+                title: {
+                    startsWith: search,
+                    mode: 'insensitive',
+                },
+            },
+            skip,
+            take,
+            orderBy: { createdAt: 'desc' },
+        });
+    },
+    async countAll(search: string) {
+        return prisma.blog.count({
+            where: {
+                title: {
+                    startsWith: search,
+                    mode: 'insensitive',
+                },
+            },
+        });
     },
     async getById(id: number) {
         return prisma.blog.findUnique({ where: { id } });
